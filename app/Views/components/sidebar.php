fdecd5f3f84473eae2afe7cb376224a7c7ffc578
Ilham134-dom <ilham.gozali2345566@gmail.com>
2025-06-14 18:34:00 +0700

    menambahkan api data transaksi pembelian

--- app/Views/components/sidebar.php
+++ app/Views/components/sidebar.php
@@ -15,42 +15,50 @@
                 <i class="bi bi-cart-check"></i>
                 <span>Keranjang</span>
             </a>
-        </li><!-- End Keranjang Nav --> 
+        </li><!-- End Keranjang Nav -->
         <?php
-        if (session()->get('role') == 'admin'){
-          ?>
+        if (session()->get('role') == 'admin') {
+        ?>
             <li class="nav-item">
-            <a class="nav-link <?php echo (uri_string() == 'produk') ? "" : "collapsed" ?>" href="produk">
-                <i class="bi bi-receipt"></i>
-                <span>Produk</span>
-            </a>
-        </li><!-- End Produk Nav -->
-        <li class="nav-item">
-            <a class="nav-link <?php echo (uri_string() == 'Kategori') ? "" : "collapsed" ?>" href="kategori">
-                <i class="bi bi-grid"></i>
-                <span>Produk Kategori</span>
-            </a>
-        </li><!-- End Home Nav -->
-        <?php
-          ?>
+                <a class="nav-link <?php echo (uri_string() == 'produk') ? "" : "collapsed" ?>" href="produk">
+                    <i class="bi bi-receipt"></i>
+                    <span>Produk</span>
+                </a>
+            </li><!-- End Produk Nav -->
+
             <li class="nav-item">
-            <a class="nav-link <?php echo (uri_string() == 'F.A.Q') ? "" : "collapsed" ?>" href="faq">
-                <i class="bi bi-receipt"></i>
-                <span>F.A.Q</span>
-            </a>
-        </li><!-- End Produk faq -->
-        <?php
-       
-          ?>
+                <a class="nav-link <?php echo (uri_string() == 'profile') ? "" : "collapsed" ?>" href="profile">
+                    <i class="bi bi-person"></i>
+                    <span>Profile</span>
+                </a>
+            </li><!-- End Profile Nav -->
+
             <li class="nav-item">
-            <a class="nav-link <?php echo (uri_string() == 'Contact') ? "" : "collapsed" ?>" href="contact">
-                <i class="bi bi-receipt"></i>
-                <span>Contact</span>
-            </a>
-        </li><!-- End Produk Nav -->
+                <a class="nav-link <?php echo (uri_string() == 'Kategori') ? "" : "collapsed" ?>" href="kategori">
+                    <i class="bi bi-grid"></i>
+                    <span>Produk Kategori</span>
+                </a>
+            </li><!-- End Home Nav -->
+            <?php
+            ?>
+            <li class="nav-item">
+                <a class="nav-link <?php echo (uri_string() == 'F.A.Q') ? "" : "collapsed" ?>" href="faq">
+                    <i class="bi bi-receipt"></i>
+                    <span>F.A.Q</span>
+                </a>
+            </li><!-- End Produk faq -->
+            <?php
+
+            ?>
+            <li class="nav-item">
+                <a class="nav-link <?php echo (uri_string() == 'Contact') ? "" : "collapsed" ?>" href="contact">
+                    <i class="bi bi-receipt"></i>
+                    <span>Contact</span>
+                </a>
+            </li><!-- End Produk Nav -->
         <?php
         }
-    
+
 
         ?>
     </ul>
